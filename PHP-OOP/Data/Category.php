8d9d9cfdc609ff9aca37b24b2293d1838df89268
Marleess <marleysumarly@gmail.com>
2022-06-30 20:50:50 +0700

    n/a

--- PHP-OOP/Data/Category.php
+++ PHP-OOP/Data/Category.php
@@ -1,10 +1,29 @@
 <?php
 
-
 class Category
 {
   private string $name;
-  private string $expensive;
+  private bool $expensive;
 
-  // getName
+  public function getName(): string
+  {
+    return $this->name;
+  }
+
+  public function setName(string $name): void
+  {
+    if (trim($name) != "") {
+      $this->name = $name;
+    }
+  }
+
+  public function isExpensive(): bool
+  {
+    return $this->expensive;
+  }
+
+  public function setExpensive(bool $expensive): void
+  {
+    $this->expensive = $expensive;
+  }
 }
