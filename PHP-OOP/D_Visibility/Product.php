681e5692837173e8dd2a13833d340b3ef4bc1fb4
Marleess (Marleess) <marleysumarly@gmail.com>
2022-10-02 13:00:28 +0700

    Visibility

---
+++ PHP-OOP/D_Visibility/Product.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace D_Visibility {
+
+  use Exception;
+
+  class Product
+  {
+
+    protected string $image;
+    protected string $name;
+    protected string $category;
+    protected int $quantity;
+    protected float $price;
+
+    // Function __construct
+    // kegunaan construct: x = new Product("$name, $address");
+    function __construct(
+      string $image,
+      string $name,
+      string $category,
+      int $quantity,
+      float $price
+    ) {
+      $this->image = $image;
+      $this->name = $name;
+      $this->category = $category;
+      $this->quantity = $quantity;
+      $this->price = $price;
+    }
+
+    // Function Show Info
+    public function showInfo(): void
+    {
+      echo "Image: $this->image" . "<br>";
+      echo "Name: $this->name" . "<br>";
+      echo "Category: $this->category" . "<br>";
+      echo "Quantity: $this->quantity" . "<br>";
+      echo "Price: " . "Rp " . number_format($this->price, 2, ",", ".") . "<br>";
+    }
+
+    // Function Max Name Input
+    public function maxStringInput(): void
+    {
+      if (strlen($this->name) > 25) {
+        throw new Exception("Inputan maksimal 10 karakter");
+      }
+    }
+
+    // Function Max Quantity Input
+    public function maxIntInput(): void
+    {
+      if ($this->quantity > 1000) {
+        throw new Exception("Inputan maksimal 1.000");
+      }
+    }
+
+    // Function Max Price Input
+    public function maxFloatInput(): void
+    {
+      if ($this->price > 1_000_000_000) {
+        throw new Exception("Inputan maksimal 1.000.000.000");
+      }
+    }
+
+    // Function __destruct
+    function __destruct()
+    {
+    }
+  }
+
+  class Food extends Product
+  {
+    public string $expiredDate;
+
+    function __construct(
+      string $image,
+      string $name,
+      string $category,
+      int $quantity,
+      float $price,
+      string $expiredDate
+    ) {
+      parent::__construct($image, $name, $category, $quantity, $price);
+      $this->expiredDate = $expiredDate;
+    }
+
+    public function showInfo(): void
+    {
+      parent::showInfo();
+      echo "Expired Date: $this->expiredDate" . "<br>";
+    }
+  }
+}
